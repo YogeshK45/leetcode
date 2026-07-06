f1f4a42b319d1b8e551bad0e622b19d0ffeca8fa
Yogesh kumar <141839046+YogeshK45@users.noreply.github.com>
2026-07-06 13:00:19 +0530

    Time: 18 ms (40.80%) | Memory: 108.3 MB (6.41%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -3,15 +3,14 @@ import java.util.*;
 class Solution {
     public boolean containsDuplicate(int[] nums) {
 
-        HashSet<Integer> set = new HashSet<>();
+       HashSet<Integer> set = new HashSet<>();
+       for(int num : nums){
+        if(set.contains(num)){
+            return true;
 
-        for (int num : nums) {
-            if (set.contains(num)) {
-                return true; 
-            }
-            set.add(num);
         }
-
-        return false;  
+         set.add(num);
+       }
+       return false;
     }
 }
